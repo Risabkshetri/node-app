7f8ec4a8e8bb266a9f4082b1aa4e37dbb9a34c46
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-08-18 23:03:25 +0530

    http  and concept of server learned

--- 02client-server/index.js
+++ 02client-server/index.js
@@ -28,10 +28,63 @@ const http = require('http');
 const fs = require('fs');
 
 const index = fs.readFileSync("index.html", 'utf-8')
+const data = JSON.parse(fs.readFileSync("data.json", 'utf-8'));
+// const product = data[0];
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html')
-    res.end(index)
+    // res.setHeader('Content-Type', 'text/html')
+    // res.end(index)
+
+    // res.setHeader('Content-Type', 'application/json')
+    // res.end(data)
+
+    if(req.url.startsWith('/product')) {
+       const id = req.url.split('/')[2]
+     const product = data.find(item => item.id === (+id))
+            res.setHeader('Content-Type', 'text/html')
+            let modifiedIndex = index.replace('**title**', product.title)
+            .replace('**thumbnail**', product.thumbnail)
+            .replace('**description**', product.description)
+            .replace('**price**', product.price)
+            .replace('**discountPercentage**', product.discountPercentage)
+            .replace('**rating**', product.rating)
+            .replace('**stock**', product.stock)
+            .replace('**brand**', product.brand)
+            .replace('**category**', product.category)
+            .replace('**availabilityStatus**', product.availabilityStatus);
+            res.end(modifiedIndex)
+            return;
+    } 
+
+    switch (req.url) {
+        case '/':
+            res.setHeader('Content-Type', 'text/html')
+            res.end(index)
+            break;
+        case '/api':
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify(data))
+            break;
+        // case '/product':
+        //     res.setHeader('Content-Type', 'text/html')
+        //     let modifiedIndex = index.replace('**title**', product.title)
+        //     .replace('**thumbnail**', product.thumbnail)
+        //     .replace('**description**', product.description)
+        //     .replace('**price**', product.price)
+        //     .replace('**discountPercentage**', product.discountPercentage)
+        //     .replace('**rating**', product.rating)
+        //     .replace('**stock**', product.stock)
+        //     .replace('**brand**', product.brand)
+        //     .replace('**category**', product.category)
+        //     .replace('**availabilityStatus**', product.availabilityStatus);
+        //     res.end(modifiedIndex)
+        //     break;
+    
+        default: 
+            res.writeHead(404, 'Not Found')
+            res.end('Not Found')
+            break;
+    }
 })
 
 server.listen(8080)
