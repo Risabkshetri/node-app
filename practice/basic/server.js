e578091e6480d078f7b570ed0a4f1363c6edf08f
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-08-27 19:16:09 +0530

    learned basic by own

---
+++ practice/basic/server.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const index = fs.readFileSync('index.html', 'utf-8');
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const server = express();
+
+//bodyParser
+server.use(express.json());
+server.use(morgan('default'))
+server.use(express.static('public'));
+
+server.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'index.html'), (err) => {
+    if (err) {
+      res.status(404).send('File not found');
+    }
+  });
+});
+
+server.listen(8080, () => {
+  console.log('server started');
+});
